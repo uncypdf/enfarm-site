952130bc3bb74414bcd31d11be6810b828ba758c
juhyun lee <juhyunlee@juhyunui-MacBookAir-2.local>
2026-05-29 18:18:33 +0900

    Add hero image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "(주)엔팜 | 면역이 방역이다!",
-  description: "주식회사 엔팜",
-  icons: {
-
-    icon: "/favicon.png",
-
-  },
-};
+  export const metadata = {
+    title: "(주)엔팜 | 면역이 방역이다!",
+    description:
+      "H-LMP 저분자 단백질, 기능성 사료 및 축산 바이오 솔루션 전문기업",
+    keywords: [
+      "엔팜",
+      "H-LMP",
+      "저분자단백질",
+      "양돈",
+      "축산",
+      "사료첨가제",
+      "에그밀",
+      "PRRS",
+      "PED",
+    ],
+    icons: {
+      icon: "/favicon.png",
+    },
+  };
 
 export default function RootLayout({
   children,
